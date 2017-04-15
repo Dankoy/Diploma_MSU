c7da23b0f5c20a7bbb5719a4740c2e5b3170cf18
Evgeny Zelenin <transingularity@gmail.com>
2017-04-15 17:26:23 +0300

    Added class for json parsing

---
+++ diploma/src/main/java/diploma/JsonMain.java
@@ -0,0 +1,17 @@
+package diploma;
+
+public class JsonMain {
+
+	private static final class Coordinate {
+
+	    private final double latitude;
+	    private final double longitude;
+
+	    private Coordinate(final double latitude, final double longitude) {
+	        this.latitude = latitude;
+	        this.longitude = longitude;
+	    }
+
+	}
+	
+}
