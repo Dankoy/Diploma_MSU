a0e2046865014147cb11100a90e5f81346832283
Evgeny Zelenin <transingularity@gmail.com>
2017-08-20 01:29:30 +0300

    Added DBSCANClusterer class which provide methods to cluster json data

---
+++ diploma/src/main/java/diploma/DBSCANClusterer.java
@@ -0,0 +1,19 @@
+package diploma;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class DBSCANClusterer {
+	
+	private double epsilon = 1f;
+	private int minimumNumbersOfClusterMembers = 2;
+	
+	private ArrayList<Coordinate> inputValues = null;
+	
+	private HashSet<Coordinate> visitedPoints = new HashSet<>();
+	
+	public static final double R = 6400; // Earth Radius In kilometers
+	
+	
+	
+}
